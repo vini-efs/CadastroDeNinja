d00f1af92f940fa5308c7a7c1457c93a3bc1d4e5
vini-efs <viniciuseduardo904@gmail.com>
2025-03-07 10:18:27 -0300

    first commit

---
+++ src/main/java/dev/java10x/CadastroNinja/CadastroNinjaApplication.java
@@ -0,0 +1,13 @@
+package dev.java10x.CadastroNinja;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastroNinjaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastroNinjaApplication.class, args);
+	}
+
+}
