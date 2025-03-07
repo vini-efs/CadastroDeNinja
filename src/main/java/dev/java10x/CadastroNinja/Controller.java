27f58536a3823ff3b92fc8bc8e1a2fde7da2dee4
vini-efs <viniciuseduardo904@gmail.com>
2025-03-07 10:50:51 -0300

    criado o controller

---
+++ src/main/java/dev/java10x/CadastroNinja/Controller.java
@@ -0,0 +1,17 @@
+package dev.java10x.CadastroNinja;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class Controller {
+
+    @GetMapping("/boasvindas")
+    public String boasVindas() {
+        return "Primeira mensagem nessa rota";
+    }
+
+
+}
