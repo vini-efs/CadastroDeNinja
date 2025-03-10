a800d11ba930117a1476afe4d8f37e1cc0423945
vini-efs <viniciuseduardo904@gmail.com>
2025-03-10 17:45:58 -0300

    feat: Cria o Service e Repository

---
+++ src/main/java/dev/java10x/CadastroNinja/Missoes/MissoesRepository.java
@@ -0,0 +1,6 @@
+package dev.java10x.CadastroNinja.Missoes;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MissoesRepository extends JpaRepository<MissoesModel, Long> {
+}
---
+++ src/main/java/dev/java10x/CadastroNinja/Missoes/MissoesServices.java
@@ -0,0 +1,4 @@
+package dev.java10x.CadastroNinja.Missoes;
+
+public class MissoesServices {
+}
