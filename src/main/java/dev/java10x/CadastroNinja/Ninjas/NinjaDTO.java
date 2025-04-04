fdadf8b1d376841e48a82eed85db19c060340946
vini-efs <viniciuseduardo904@gmail.com>
2025-04-04 18:31:27 -0300

    Chore: inclui a versao do DTO pela entidade e mapeia manualmente no NinjaMapper os caminhos entidade x DTO

--- src/main/java/dev/java10x/CadastroNinja/Ninjas/NinjaDTO.java
+++ src/main/java/dev/java10x/CadastroNinja/Ninjas/NinjaDTO.java
@@ -1,4 +1,57 @@
 package dev.java10x.CadastroNinja.Ninjas;
 
+import dev.java10x.CadastroNinja.Missoes.MissoesModel;
+import lombok.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
 public class NinjaDTO {
+
+    private long id;
+    private String nome;
+    private String email;
+    private int idade;
+    private String rank;
+    private MissoesModel missoes;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public String getRank() {
+        return rank;
+    }
+
+    public void setRank(String rank) {
+        this.rank = rank;
+    }
+
+    public MissoesModel getMissoes() {
+        return missoes;
+    }
+
+    public void setMissoes(MissoesModel missoes) {
+        this.missoes = missoes;
+    }
 }
+
--- src/main/java/dev/java10x/CadastroNinja/Ninjas/NinjaMapper.java
+++ src/main/java/dev/java10x/CadastroNinja/Ninjas/NinjaMapper.java
@@ -1,4 +1,33 @@
 package dev.java10x.CadastroNinja.Ninjas;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class NinjaMapper {
+
+    public NinjaModel map(NinjaDTO ninjaDTO) {
+
+        NinjaModel ninjaModel = new NinjaModel();
+        ninjaModel.setNome(ninjaDTO.getNome());
+        ninjaModel.setEmail(ninjaDTO.getEmail());
+        ninjaModel.setIdade(ninjaDTO.getIdade());
+        ninjaModel.setRank(ninjaDTO.getRank());
+        ninjaModel.setMissoes(ninjaDTO.getMissoes());
+
+        return ninjaModel;
+    }
+
+    public NinjaDTO map(NinjaModel ninjaModel) {
+
+        NinjaDTO ninjaDTO = new NinjaDTO();
+        ninjaDTO.setNome(ninjaModel.getNome());
+        ninjaDTO.setEmail(ninjaModel.getEmail());
+        ninjaDTO.setIdade(ninjaModel.getIdade());
+        ninjaDTO.setMissoes(ninjaModel.getMissoes());
+        ninjaDTO.setRank(ninjaModel.getRank());
+
+        return ninjaDTO;
+    }
+
+
 }
