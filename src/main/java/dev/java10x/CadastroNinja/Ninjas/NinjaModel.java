600a23cb8b01be4a276db1e16e8e60cd9074d4b0
vini-efs <viniciuseduardo904@gmail.com>
2025-04-04 18:28:37 -0300

    fix: Inclui a coluna de rank na entidade, porem ja estava incluida na migration V2

--- src/main/java/dev/java10x/CadastroNinja/Ninjas/NinjaModel.java
+++ src/main/java/dev/java10x/CadastroNinja/Ninjas/NinjaModel.java
@@ -3,7 +3,6 @@ package dev.java10x.CadastroNinja.Ninjas;
 import dev.java10x.CadastroNinja.Missoes.MissoesModel;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
-import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.util.List;
@@ -14,7 +13,6 @@ import java.util.List;
 * */
 @Entity
 @Table(name = "tb_cadastro")
-@Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class NinjaModel {
@@ -30,9 +28,60 @@ public class NinjaModel {
 
     private int idade;
 
+    @Column (name = "rank")
+    private String rank;
+
     // ManyToOne: Relacionamento entre tabelas: Nesse caso N -> 1
     @ManyToOne
     @JoinColumn(name = "missoes_id") // Chave Estrangeira
     private MissoesModel missoes;
 
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public String getRank() {
+        return rank;
+    }
+
+    public void setRank(String rank) {
+        this.rank = rank;
+    }
+
+    public MissoesModel getMissoes() {
+        return missoes;
+    }
+
+    public void setMissoes(MissoesModel missoes) {
+        this.missoes = missoes;
+    }
 }
+
